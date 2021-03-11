b94dc0b6a899a2c12ac0f6fe2657b82ee540f66f
Voltstro <me@voltstro.dev>
2021-03-11 14:43:14 +1000

    Some XML and comments

--- src/CefBrowser/Runtime/WebBrowserUtils.cs
+++ src/CefBrowser/Runtime/WebBrowserUtils.cs
@@ -4,10 +4,22 @@ using UnityEngine.UI;
 
 namespace UnityWebBrowser
 {
+	/// <summary>
+	///		Provides Utils to be used by the web browser
+	/// </summary>
     public static class WebBrowserUtils
     {
+#if UNITY_EDITOR
+	    /// <summary>
+		///		The name of the package that the web browser is contained in
+		/// </summary>
 	    public const string PackageName = "dev.voltstro.unitywebbrowser";
+#endif
 
+		/// <summary>
+		///		Gets the folder that the cef process application lives in
+		/// </summary>
+		/// <returns></returns>
 	    public static string GetCefProcessPath()
 	    {
 #if UNITY_EDITOR
@@ -17,6 +29,10 @@ namespace UnityWebBrowser
 #endif
 	    }
 
+		/// <summary>
+		///		Get a direct path to the cef browser process application
+		/// </summary>
+		/// <returns></returns>
 	    public static string GetCefProcessApplication()
 	    {
 		    return $"{GetCefProcessPath()}/CefBrowserProcess.exe";
@@ -24,6 +40,9 @@ namespace UnityWebBrowser
 
 		/// <summary>
 		///		Gets the local position delta (0 -> 1) from a screen position on a <see cref="RawImage"/>
+		///		from a top-left origin point
+		///		<para>To calculate the pixel position,
+		///		do <see cref="Vector2.x"/> * [Desired height] and <see cref="Vector2.y"/> * [Desired Width]</para>
 		/// </summary>
 		/// <param name="image"><see cref="RawImage"/> that you want to calculate the local position on</param>
 		/// <param name="screenPosition">The screen position</param>
@@ -31,6 +50,14 @@ namespace UnityWebBrowser
 		/// <returns></returns>
 	    public static bool GetScreenPointToLocalPositionDeltaOnImage(RawImage image, Vector2 screenPosition, out Vector2 position)
 	    {
+			//This was a pain in the ass to figure out how to do, I never want anything to do with mouses and UI elements ever again.
+
+			//The main pain was that CEF uses a top left origin point, not center like Unity, but turns out its dog shit
+			//simple to do and that I am terrible at maths
+
+			//There probs something here that could be done better, if you know, send in a PR
+			//Based off: http://answers.unity.com/answers/1455168/view.html
+
 		    position = new Vector2();
 		    RectTransform uiImageObjectRect = image.rectTransform;
 		    if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(uiImageObjectRect, screenPosition, null,
