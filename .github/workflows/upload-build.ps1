4f4f5501f6956c9ef80ea2b45f7e7c768bb0bba3
Voltstro <me@voltstro.dev>
2021-05-23 01:21:10 +1000

    PR workflow
    Some comments

--- .github/workflows/upload-build.ps1
+++ .github/workflows/upload-build.ps1
@@ -1,11 +1,15 @@
+#Clone the repo
 git clone https://gitlab.com/Voltstro-Studios/WebBrowser/Package.git
 
+#Detect if there is a change in the package.json files
 if(Compare-Object -ReferenceObject $(Get-Content UnityWebBrowserSource/src/CefBrowser/package.json) -DifferenceObject $(Get-Content Package/package.json))
 {
     Write-Host "Changes in package.json, deploying..."
 
+    #Remove everything excet the git folder to start from a clean state
     Remove-Item -Recurse Package/* -Exclude .git
 
+    #Lots of copying
     Get-ChildItem -Path UnityWebBrowserSource/src/CefBrowser/ | Copy-Item -Destination Package/ -Recurse -Container -Force -PassThru
 
     Get-ChildItem -Path Linux-CefBrowserProcess/ | Copy-Item -Destination Package/Plugins/CefBrowser/linux-x64 -Recurse -Container -Force -PassThru
@@ -13,13 +17,14 @@ if(Compare-Object -ReferenceObject $(Get-Content UnityWebBrowserSource/src/CefBr
 
     Remove-Item -Path Package/Plugins/CefBrowser/.gitignore -Force
 
-    #Set-Location -path "Package/"
-    #git config --global user.name "Voltstro"
-    #git config --global user.email "me@voltstro.dev"
-    #git remote set-url origin git@gitlab.com:Voltstro-Studios/WebBrowser/Package.git
-    #git add *
-    #git commit -m "Update"
-    #git push
+    #Push the changes with SSH
+    Set-Location -path "Package/"
+    git config --global user.name "Voltstro"
+    git config --global user.email "me@voltstro.dev"
+    git remote set-url origin git@gitlab.com:Voltstro-Studios/WebBrowser/Package.git
+    git add *
+    git commit -m "Update"
+    git push
 }
 else 
 {
