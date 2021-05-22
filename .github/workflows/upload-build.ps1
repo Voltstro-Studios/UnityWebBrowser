5d05efd856f6959d60d9ee09f0d933024308e761
Voltstro <me@voltstro.dev>
2021-05-23 00:38:02 +1000

    Remove gitignore

--- .github/workflows/upload-build.ps1
+++ .github/workflows/upload-build.ps1
@@ -4,6 +4,8 @@ Get-ChildItem -Path UnityWebBrowserSource/src/CefBrowser/ | Copy-Item -Destinati
 Get-ChildItem -Path Linux-CefBrowserProcess/ | Copy-Item -Destination Package/Plugins/CefBrowser/linux-x64 -Recurse -Container -Force -PassThru
 Get-ChildItem -Path Windows-CefBrowserProcess/ | Copy-Item -Destination Package/Plugins/CefBrowser/win-x64 -Recurse -Container -Force -PassThru
 
+Remove-Item -Path Package/Plugins/CefBrowser/.gitignore -Force
+
 Set-Location -path "Package/"
 git config --global user.name "Voltstro"
 git config --global user.email "me@voltstro.dev"
