fd691b0ababa021811f65480053da169b6ef60f4
Voltstro <me@voltstro.dev>
2021-05-27 13:09:29 +1000

    On popup load it in window

--- src/CefBrowserProcess/Browser/BrowserProcessCEFLifespanHandler.cs
+++ src/CefBrowserProcess/Browser/BrowserProcessCEFLifespanHandler.cs
@@ -15,6 +15,15 @@ namespace CefBrowserProcess.Browser
             AfterCreated?.Invoke(browser);
         }
 
+        protected override bool OnBeforePopup(CefBrowser browser, CefFrame frame, string targetUrl, string targetFrameName,
+            CefWindowOpenDisposition targetDisposition, bool userGesture, CefPopupFeatures popupFeatures,
+            CefWindowInfo windowInfo, ref CefClient client, CefBrowserSettings settings, ref CefDictionaryValue extraInfo,
+            ref bool noJavascriptAccess)
+        {
+            frame.LoadUrl(targetUrl);
+            return true;
+        }
+
         protected override bool DoClose(CefBrowser browser)
         {
             return false;
