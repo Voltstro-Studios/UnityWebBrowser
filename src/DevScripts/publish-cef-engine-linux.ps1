d81293c313263da65abafddcef4e59a2785816f1
Voltstro <me@voltstro.dev>
2021-06-12 19:39:44 +1000

    Move UnityWebBrowser.Engine.Cef to its own package

--- src/DevScripts/publish-cef-engine-linux.ps1
+++ src/DevScripts/publish-cef-engine-linux.ps1
@@ -7,4 +7,4 @@ else
     dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true --self-contained true --nologo
 }
 
-Copy-Item -Path "../UnityWebBrowser.Engine.Cef/bin/ReleaseUnity/linux-x64/publish/*" -Destination "../Packages/UnityWebBrowser/Plugins/CefBrowser/linux-x64/" -Recurse -Force -PassThru
+Copy-Item -Path "../UnityWebBrowser.Engine.Cef/bin/ReleaseUnity/linux-x64/publish/*" -Destination "../Packages/UnityWebBrowser.Engine.Cef.Linux-x64/Plugins/linux-x64/" -Recurse -Force -PassThru
--- src/Packages/UnityWebBrowser.Engine.Cef/Editor/CefWebBrowser.cs
+++ src/Packages/UnityWebBrowser.Engine.Cef/Editor/CefWebBrowser.cs
@@ -7,14 +7,15 @@ namespace UnityWebBrowser.Engine.Cef.Editor
     [InitializeOnLoad]
     public static class CefWebBrowser
     {
-        private const string CefBrowserPackageName = "dev.voltstro.unitywebbrowser";
+        private const string CefBrowserEngineWindowsPackageName = "dev.voltstro.unitywebbrowser.engine.cef.win.x64";
+        private const string CefBrowserEngineLinuxPackageName = "dev.voltstro.unitywebbrowser.engine.cef.linux.x64";
         
         static CefWebBrowser()
         {
             Dictionary<BuildTarget, string> buildFiles = new Dictionary<BuildTarget, string>
             {
-                {BuildTarget.StandaloneWindows64, $"Packages/{CefBrowserPackageName}/Plugins/CefBrowser/win-x64/"},
-                {BuildTarget.StandaloneLinux64, $"Packages/{CefBrowserPackageName}/Plugins/CefBrowser/linux-x64/"}
+                {BuildTarget.StandaloneWindows64, $"Packages/{CefBrowserEngineWindowsPackageName}/Plugins/win-x64/"},
+                {BuildTarget.StandaloneLinux64, $"Packages/{CefBrowserEngineLinuxPackageName}/Plugins/linux-x64/"}
             };
 
             BrowserEngineManager.AddBrowserEngine(new BrowserEngine("Cef Browser Engine", "UnityWebBrowser.Engine.Cef", buildFiles));
