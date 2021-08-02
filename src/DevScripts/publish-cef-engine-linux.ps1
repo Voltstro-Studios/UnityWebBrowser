5023e8f35d79a730d80638585dad3b57731cb7ee
Voltstro <me@voltstro.dev>
2021-08-02 22:52:07 +1000

    Welcome back, trimming
    Copy VoltRpc dll to Unity project on build

--- src/DevScripts/publish-cef-engine-linux.ps1
+++ src/DevScripts/publish-cef-engine-linux.ps1
@@ -1,10 +1,10 @@
 if($IsLinux)
 {
-    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:PublishReadyToRun=true --self-contained true --nologo
+    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained true --nologo
 }
 else
 {
-    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true --self-contained true --nologo
+    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:PublishTrimmed=true --self-contained true --nologo
 }
 
 Copy-Item -Path "../UnityWebBrowser.Engine.Cef/bin/ReleaseUnity/linux-x64/publish/*" -Destination "../Packages/UnityWebBrowser.Engine.Cef.Linux-x64/Plugins/linux-x64/" -Recurse -Force -PassThru
--- src/DevScripts/publish-cef-engine-windows.ps1
+++ src/DevScripts/publish-cef-engine-windows.ps1
@@ -1,10 +1,10 @@
 if($IsWindows)
 {
-    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true --self-contained true --nologo
+    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:PublishTrimmed=true -p:PublishReadyToRun=true --self-contained true --nologo
 }
 else
 {
-    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true --self-contained true --nologo
+    dotnet publish ../UnityWebBrowser.Engine.Cef/UnityWebBrowser.Engine.Cef.csproj -c ReleaseUnity -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:PublishTrimmed=true --self-contained true --nologo
 }
 
 Copy-Item -Path "../UnityWebBrowser.Engine.Cef/bin/ReleaseUnity/win-x64/publish/*" -Destination "../Packages/UnityWebBrowser.Engine.Cef.Win-x64/Plugins/win-x64/" -Recurse -Force -PassThru
