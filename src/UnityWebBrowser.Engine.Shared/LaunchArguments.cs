6b9c57aa0de23e4374e98703dcf35f0e5219c991
Voltstro <me@voltstro.dev>
2022-01-03 02:59:07 +1000

    Use init

--- src/UnityWebBrowser.Engine.Shared/LaunchArguments.cs
+++ src/UnityWebBrowser.Engine.Shared/LaunchArguments.cs
@@ -3,7 +3,6 @@ using UnityWebBrowser.Shared;
 
 namespace UnityWebBrowser.Engine.Shared
 {
-    // ReSharper disable UnusedAutoPropertyAccessor.Global
     /// <summary>
     ///     Launch arguments for the app
     /// </summary>
@@ -12,97 +11,96 @@ namespace UnityWebBrowser.Engine.Shared
         /// <summary>
         ///     The initial URL for the browser
         /// </summary>
-        public string InitialUrl { get; set; }
+        public string InitialUrl { get; init; }
 
         /// <summary>
         ///     The initial width of the browser
         /// </summary>
-        public int Width { get; set; }
+        public int Width { get; init; }
 
         /// <summary>
         ///     The initial height of the browser
         /// </summary>
-        public int Height { get; set; }
+        public int Height { get; init; }
 
         /// <summary>
         ///     Is JavaScript enabled
         /// </summary>
-        public bool JavaScript { get; set; }
+        public bool JavaScript { get; init; }
 
         /// <summary>
         ///     Is Web RTC enabled
         /// </summary>
-        public bool WebRtc { get; set; }
+        public bool WebRtc { get; init; }
 
         /// <summary>
         ///     The port to use for remote debugging
         /// </summary>
-        public int RemoteDebugging { get; set; }
+        public int RemoteDebugging { get; init; }
 
         /// <summary>
         ///     Background color (red)
         /// </summary>
-        public byte Bcr { get; set; }
+        public byte Bcr { get; init; }
 
         /// <summary>
         ///     Background color (green)
         /// </summary>
-        public byte Bcg { get; set; }
+        public byte Bcg { get; init; }
 
         /// <summary>
         ///     Background color (blue)
         /// </summary>
-        public byte Bcb { get; set; }
+        public byte Bcb { get; init; }
 
         /// <summary>
         ///     Background color (alpha)
         /// </summary>
-        public byte Bca { get; set; }
+        public byte Bca { get; init; }
 
         /// <summary>
         ///     The path you should use for your cache
         /// </summary>
-        public FileInfo CachePath { get; set; }
+        public FileInfo CachePath { get; init; }
 
         /// <summary>
         ///     Should we use a proxy or direct
         /// </summary>
-        public bool ProxyEnabled { get; set; }
+        public bool ProxyEnabled { get; init; }
 
         /// <summary>
         ///     Username of the proxy
         /// </summary>
-        public string ProxyUsername { get; set; }
+        public string ProxyUsername { get; init; }
 
         /// <summary>
         ///     Password of the proxy
         /// </summary>
-        public string ProxyPassword { get; set; }
+        public string ProxyPassword { get; init; }
 
         /// <summary>
         ///     Pipes or not
         /// </summary>
-        public bool Pipes { get; set; }
+        public bool Pipes { get; init; }
 
         /// <summary>
         ///     In location (Either the pipe name or port)
         /// </summary>
-        public string InLocation { get; set; }
+        public string InLocation { get; init; }
 
         /// <summary>
         ///     Out location (Either the pipe name or port)
         /// </summary>
-        public string OutLocation { get; set; }
+        public string OutLocation { get; init; }
 
         /// <summary>
         ///     The path you should log browser events to
         /// </summary>
-        public FileInfo LogPath { get; set; }
+        public FileInfo LogPath { get; init; }
 
         /// <summary>
         ///     What is the log severity
         /// </summary>
-        public LogSeverity LogSeverity { get; set; }
+        public LogSeverity LogSeverity { get; init; }
     }
-    // ReSharper restore UnusedAutoPropertyAccessor.Global
 }
