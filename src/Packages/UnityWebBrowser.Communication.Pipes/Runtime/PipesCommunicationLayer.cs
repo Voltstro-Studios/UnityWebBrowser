5bd164ee466ceeea2b8cfe3df67b0c9283dddfaa
Voltstro <me@voltstro.dev>
2022-01-13 22:25:32 +1000

    Bit of cleanup

--- src/Packages/UnityWebBrowser.Communication.Pipes/Runtime/PipesCommunicationLayer.cs
+++ src/Packages/UnityWebBrowser.Communication.Pipes/Runtime/PipesCommunicationLayer.cs
@@ -4,10 +4,20 @@ using VoltRpc.Communication;
 
 namespace UnityWebBrowser.Communication.Pipes
 {
+    /// <summary>
+    ///     <see cref="CommunicationLayer"/> using <see cref="PipesHost"/> and <see cref="PipesClient"/>
+    /// </summary>
     [CreateAssetMenu(fileName = "Pipes Communication Layer", menuName = "UWB/Pipes Communication Layer")]
-    public class PipesCommunicationLayer : CommunicationLayer
+    public sealed class PipesCommunicationLayer : CommunicationLayer
     {
+        /// <summary>
+        ///     What pipe name to communicate in
+        /// </summary>
         public string inPipeName = "UnityWebBrowserIn";
+        
+        /// <summary>
+        ///     What pipe name to communicate out
+        /// </summary>
         public string outPipeName = "UnityWebBrowserOut";
 
         public override Client CreateClient()
--- src/Packages/UnityWebBrowser.Engine.Cef/Editor/CefWebBrowser.cs
+++ src/Packages/UnityWebBrowser.Engine.Cef/Editor/CefWebBrowser.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityWebBrowser.Editor;
 using UnityWebBrowser.Editor.EngineManagement;
 
 namespace UnityWebBrowser.Engine.Cef.Editor
@@ -14,19 +13,19 @@ namespace UnityWebBrowser.Engine.Cef.Editor
 
         static CefWebBrowser()
         {
-            Dictionary<BuildTarget, string> buildFiles = new Dictionary<BuildTarget, string>();
-            
+            Dictionary<BuildTarget, string> buildFiles = new();
+
             string windowsPath = $"Packages/{CefBrowserEngineWindowsPackageName}/Engine~/";
             string linuxPath = $"Packages/{CefBrowserEngineLinuxPackageName}/Engine~/";
-            
-            if(Directory.Exists(windowsPath))
+
+            if (Directory.Exists(windowsPath))
                 buildFiles.Add(BuildTarget.StandaloneWindows64, windowsPath);
-            if(Directory.Exists(linuxPath))
+            if (Directory.Exists(linuxPath))
                 buildFiles.Add(BuildTarget.StandaloneLinux64, linuxPath);
-            
-            if(buildFiles.Count > 0)
+
+            if (buildFiles.Count > 0)
                 BrowserEngineManager.AddEngine(new BrowserEngine("Cef Browser Engine", "UnityWebBrowser.Engine.Cef",
-                buildFiles));
+                    buildFiles));
         }
     }
 }
