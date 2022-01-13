6a8a7e3bb421982d88f223d53e37e88ec51e1bd6
Voltstro <me@voltstro.dev>
2022-01-13 22:27:36 +1000

    Another sync

--- src/DevScripts/sync-package-main.ps1
+++ src/DevScripts/sync-package-main.ps1
@@ -1,1 +1,3 @@
 Copy-Item -Path "../../LICENSE.md" -Destination "../Packages/UnityWebBrowser/LICENSE.md" -Force -PassThru
+Copy-Item -Path "../../LICENSE.md" -Destination "../Packages/UnityWebBrowser.Communication.Pipes/LICENSE.md" -Force -PassThru
+Copy-Item -Path "../../LICENSE.md" -Destination "../Packages/UnityWebBrowser.Unix-Support/LICENSE.md" -Force -PassThru
