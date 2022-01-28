1f3f7118327ddf146a8cac6099004265f3d410c9
Voltstro <me@voltstro.dev>
2022-01-29 02:27:10 +1000

    Update

--- src/setup-all.ps1
+++ src/setup-all.ps1
@@ -1,3 +1,9 @@
+# Setup and build pages project first
+Push-Location "$PSScriptRoot/UnityWebBrowser.Pages/"
+& yarn install
+& yarn run build
+
+# Run Scripts
 Push-Location "$PSScriptRoot/DevScripts/"
 
 & "$PSScriptRoot/DevScripts/download-all.ps1"
