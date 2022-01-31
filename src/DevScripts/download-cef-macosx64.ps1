f19268b1252c9e53b3ef96fbb23baf0edc56a43f
Volstro <me@voltstro.dev>
2022-01-31 20:55:13 +1000

    Rename cef for MacOS

--- src/DevScripts/download-cef-macosx64.ps1
+++ src/DevScripts/download-cef-macosx64.ps1
@@ -1,3 +1,5 @@
 & "$PSScriptRoot/download-cef-base.ps1" macosx64 -IncludeResources $false
 
+Rename-Item "../ThirdParty/Libs/cef/macosx64/Chromium Embedded Framework.framework/Chromium Embedded Framework" "libcef.dylib"
+
 Write-Output "Done!"
