f42042f2719a9c7ab6bddeb61d3842dcf83652ac
Konijima <mathieugermain1991@gmail.com>
2022-07-20 09:09:37 -0400

    #97 Disable inputs - Modifications

--- src/Packages/UnityWebBrowser/Runtime/Core/RawImageUwbClientInputHandler.cs
+++ src/Packages/UnityWebBrowser/Runtime/Core/RawImageUwbClientInputHandler.cs
@@ -24,14 +24,17 @@ namespace UnityWebBrowser.Core
         [Tooltip("The input handler to use")]
         public WebBrowserInputHandler inputHandler;
 
-        [Tooltip("Disable the usage of mouse and keyboard")]
-        public bool disableAllControls;
+        /// <summary>
+        ///     Disable usage of mouse
+        /// </summary>
+        [Tooltip("Disable usage of mouse")]
+        public bool disableMouseInputs;
 
-        [Tooltip("Disable the usage of mouse")]
-        public bool disableMouse;
-
-        [Tooltip("Disable the usage of keyboard")]
-        public bool disableKeyboard;
+        /// <summary>
+        ///     Disable usage of keyboard
+        /// </summary>
+        [Tooltip("Disable usage of keyboard")]
+        public bool disableKeyboardInputs;
 
         private Coroutine keyboardAndMouseHandlerCoroutine;
         private Vector2 lastSuccessfulMousePositionSent;
@@ -103,32 +106,38 @@ namespace UnityWebBrowser.Core
                 if (!browserClient.IsConnected)
                     continue;
 
-                if(disableAllControls)
+                if (disableMouseInputs && disableKeyboardInputs)
                     continue;
 
                 if (GetMousePosition(out Vector2 pos))
                 {
-                    //Mouse position
-                    if (!disableMouse && lastSuccessfulMousePositionSent != pos)
+                    if (!disableMouseInputs)
                     {
-                        browserClient.SendMouseMove(pos);
-                        lastSuccessfulMousePositionSent = pos;
+                        //Mouse position
+                        if (lastSuccessfulMousePositionSent != pos)
+                        {
+                            browserClient.SendMouseMove(pos);
+                            lastSuccessfulMousePositionSent = pos;
+                        }
+
+                        //Mouse scroll
+                        float scroll = inputHandler.GetScroll();
+                        scroll *= browserClient.BrowserTexture.height;
+
+                        if (!disableMouseInputs && scroll != 0)
+                            browserClient.SendMouseScroll(pos, (int) scroll);
                     }
-
-                    //Mouse scroll
-                    float scroll = inputHandler.GetScroll();
-                    scroll *= browserClient.BrowserTexture.height;
-
-                    if (!disableMouse && scroll != 0)
-                        browserClient.SendMouseScroll(pos, (int) scroll);
                     
-                    //Input
-                    WindowsKey[] keysDown = inputHandler.GetDownKeys();
-                    WindowsKey[] keysUp = inputHandler.GetUpKeys();
-                    string inputBuffer = inputHandler.GetFrameInputBuffer();
+                    if (!disableKeyboardInputs)
+                    {
+                        //Input
+                        WindowsKey[] keysDown = inputHandler.GetDownKeys();
+                        WindowsKey[] keysUp = inputHandler.GetUpKeys();
+                        string inputBuffer = inputHandler.GetFrameInputBuffer();
                     
-                    if(!disableKeyboard && (keysDown.Length > 0 || keysUp.Length > 0 || inputBuffer.Length > 0))
-                        browserClient.SendKeyboardControls(keysDown, keysUp, inputBuffer);
+                        if(keysDown.Length > 0 || keysUp.Length > 0 || inputBuffer.Length > 0)
+                            browserClient.SendKeyboardControls(keysDown, keysUp, inputBuffer);
+                    }
                 }
             }
             
@@ -137,7 +146,7 @@ namespace UnityWebBrowser.Core
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            if (disableAllControls || disableMouse)
+            if (disableMouseInputs)
                 return;
 
             MouseClickType clickType = eventData.button switch
@@ -154,7 +163,7 @@ namespace UnityWebBrowser.Core
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            if (disableAllControls || disableMouse)
+            if (disableMouseInputs)
                 return;
 
             MouseClickType clickType = eventData.button switch
