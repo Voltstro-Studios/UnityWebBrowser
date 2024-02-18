5d319e55af721356f1443996eeb6f32684efa559
Voltstro <me@voltstro.dev>
2024-02-18 16:50:15 +1000

    Bump version

--- src/Packages/UnityWebBrowser/Runtime/AssemblyInfo.cs
+++ src/Packages/UnityWebBrowser/Runtime/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2")]
-[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
 
 [assembly: InternalsVisibleTo("VoltstroStudios.UnityWebBrowser.Prj")]
 [assembly: InternalsVisibleTo("VoltstroStudios.UnityWebBrowser.Editor")]
